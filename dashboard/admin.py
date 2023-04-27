5832dbe2ea1b46089c10e5db8189656c4e4210ca
Rehman Khan <rehman.khan.300a@gmail.com>
2023-04-27 17:24:21 +0530

    implemented login/logout/register

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . import Author
 
-class AuthorAdmin(admin.ModelAdmin):
+
+
     
-admin.site.register(Author, AuthorAdmin)
+
 
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -17,9 +17,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import home,register,login_user,logout_user
 urlpatterns = [
-path('', home,name="home"),
-# path('login/', auth_views.LoginView.as_view(), name='login'),
-path("register/", register, name="register"),
-path("login_user", login_user, name="login_user"),
-path("logout_user", logout_user, name="logout_user"),
+    path('', home,name="home"),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    path("register/", register, name="register"),
+    path("login_user/", login_user, name="login_user"),
+    path("logout_user/", logout_user, name="logout_user"),
 ]
